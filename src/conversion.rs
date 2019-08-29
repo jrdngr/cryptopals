0b90e636dc99f2c8d3daac178a87f51d7fffaec2
Jordan Grace <jordan@righteye.com>
2019-08-29 16:33:47 -0400

    Stubbed out some functions

--- src/conversion.rs
+++ src/conversion.rs
@@ -1,1 +1,28 @@
+pub fn from_hex(s: &str) -> Result<Vec<u8>, ()> {
+    let bytes = s.as_bytes();
+    Ok(Vec::from(bytes))
+}
 
+pub fn from_base64(s: &str) -> Result<Vec<u8>, ()> {
+    let bytes = s.as_bytes();
+    Ok(Vec::from(bytes))
+}
+
+pub fn print_hex(value: &[u8]) {
+    dbg!(value);
+}
+
+pub fn print_base64(value: &[u8]) {
+    dbg!(value);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_hex() {
+        let result = from_hex("0123ABC").unwrap();
+        dbg!(result);
+    }
+}
