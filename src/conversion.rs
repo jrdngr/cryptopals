6b7af671fdebab54ee5290092eda80953aa7f9c9
Jordan Grace <jordan.t.grace@gmail.com>
2019-09-14 15:33:31 -0400

    Hex conversion is pretty close

--- src/conversion.rs
+++ src/conversion.rs
@@ -1,4 +1,5 @@
 use std::io::{Read, BufReader};
+use std::collections::HashMap;
 
 use lazy_static::lazy_static;
 
@@ -15,6 +16,37 @@ lazy_static! {
             '4', '5', '6', '7', '8', '9', '+', '/',
         ] 
     };
+
+    static ref ASCII_HEX_TABLE: HashMap<u8, u8> = {
+        let mut table = HashMap::new();
+
+        table.insert(48, 0);
+        table.insert(49, 1);
+        table.insert(50, 2);
+        table.insert(51, 3);
+        table.insert(52, 4);
+        table.insert(53, 5);
+        table.insert(54, 6);
+        table.insert(55, 7);
+        table.insert(56, 8);
+        table.insert(57, 9);
+
+        table.insert(65, 10);
+        table.insert(66, 11);
+        table.insert(67, 12);
+        table.insert(68, 13);
+        table.insert(69, 14);
+        table.insert(70, 15);
+
+        table.insert(97, 10);
+        table.insert(98, 11);
+        table.insert(99, 12);
+        table.insert(100, 13);
+        table.insert(101, 14);
+        table.insert(102, 15);
+
+        table
+    };
 }
 
 const MASK_1: u8 = 0b1111_1100;
@@ -27,7 +59,18 @@ pub fn string_to_base64(string: &str) -> String {
 }
 
 pub fn hex_to_base64(hex_string: &str) -> String {
-    unimplemented!()
+    let hex_bytes: Vec<u8> = hex_string
+        .bytes()
+        .map(|b| ASCII_HEX_TABLE[&b])
+        .collect();
+    
+    let bytes: Vec<u8> = hex_bytes
+        .as_slice()
+        .chunks(2)
+        .map(|c| (c[0] << 4) | (c[1]))
+        .collect();
+
+    bytes_to_base64(&bytes)
 }
 
 pub fn bytes_to_base64(bytes: &[u8]) -> String {
@@ -57,16 +100,7 @@ pub fn bytes_to_base64(bytes: &[u8]) -> String {
         result.push(working_buffer[2] & MASK_3);
     }
 
-    print_bits(&result);
-
     result.into_iter().map(|c| BASE_64_TABLE[c as usize]).collect()
-}
-
-fn print_bits(bytes: &[u8]) {
-    for byte in bytes {
-        print!("{:08b}, ", byte);
-    }
-    println!();
 }
 
 #[cfg(test)]
--- src/set1.rs
+++ src/set1.rs
@@ -8,5 +8,8 @@ use crate::conversion::hex_to_base64;
 // pretty-printing. 
 
 pub fn challenge_1() {
-    println!("{}", hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"));
+    assert_eq!(
+        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
+        hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+    );
 }
