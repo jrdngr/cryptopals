ed65e4af9808def464b82f2d8517851614e4917f
Jordan Grace <jordan.t.grace@gmail.com>
2019-09-22 13:02:55 -0400

    Finished 1-2 for real

--- src/byte_operations.rs
+++ src/byte_operations.rs
@@ -1,3 +1,6 @@
-pub fn bytes_xor(_bytes: &[u8]) -> Vec<u8> {
-    Vec::new()
+pub fn bytes_xor(bytes1: &[u8], bytes2: &[u8]) -> Vec<u8> {
+    bytes1.iter()
+        .zip(bytes2.iter())
+        .map(|(b1, b2)| b1 ^ b2)
+        .collect()
 }
--- src/set1.rs
+++ src/set1.rs
@@ -1,4 +1,6 @@
 use crate::conversion::base64::hex_to_base64;
+use crate::conversion::hex::{hex_string_to_bytes, bytes_to_hex_string};
+use crate::byte_operations::bytes_xor;
 
 // Convert hex to base64
 // input: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
@@ -8,10 +10,12 @@ use crate::conversion::base64::hex_to_base64;
 // pretty-printing.
 #[test]
 pub fn challenge1() {
-        let test_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-        let expected_output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+    let expected_output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
-        assert_eq!(hex_to_base64(test_string), expected_output);
+    let test_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let result = hex_to_base64(test_string);
+
+    assert_eq!(result, expected_output);
 }
 
 
@@ -22,8 +26,15 @@ pub fn challenge1() {
 // 686974207468652062756c6c277320657965
 // ... should produce:
 // 746865206b696420646f6e277420706c6179
-
+#[test]
 pub fn challenge_2() {
-    let _string_1 = "1c0111001f010100061a024b53535009181c";
-    let _string_2 = "686974207468652062756c6c277320657965";
+    let expected_output = "746865206b696420646f6e277420706c6179";
+
+    let bytes1 = hex_string_to_bytes("1c0111001f010100061a024b53535009181c");
+    let bytes2 = hex_string_to_bytes("686974207468652062756c6c277320657965");
+    
+    let result_bytes = bytes_xor(&bytes1, &bytes2);
+    let result = bytes_to_hex_string(&result_bytes);
+     
+    assert_eq!(result, expected_output);
 }
