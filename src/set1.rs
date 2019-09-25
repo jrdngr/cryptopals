6a433dc3a39c6c74bf5aa07a996b9ad9f74990b9
Jordan Grace <jordan@righteye.com>
2019-09-25 18:03:40 -0400

    Finished challenge 3

--- src/set1.rs
+++ src/set1.rs
@@ -1,6 +1,6 @@
-use crate::conversion::base64::{hex_to_base64, bytes_to_base64};
+use crate::conversion::base64::hex_to_base64;
 use crate::conversion::hex::{hex_string_to_bytes, bytes_to_hex_string};
-use crate::byte_operations::bytes_xor;
+use crate::byte_operations::{bytes_xor, bytes_xor_single_byte};
 
 // Convert hex to base64
 // input: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
@@ -46,16 +46,38 @@ pub fn challenge_2() {
 // How? Devise some method for "scoring" a piece of English plaintext. Character frequency is a good metric. Evaluate each output and choose the one with the best score. 
 #[test]
 pub fn challenge_3() {
-    let encoded = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    use std::collections::HashMap;
 
-    let bytes = hex_string_to_bytes(encoded);
-    let len = bytes.len();
+    let bytes = hex_string_to_bytes("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
     
-    for val in 0..15 {
-        let other: Vec<u8> = vec![val; len];
-        let result = bytes_xor(&bytes, &other);
-        let result_string = std::str::from_utf8(&result).unwrap();
-        println!("{}", val);
-        println!("{}", result_string);
+    let mut result: HashMap<String, usize> = HashMap::new();
+
+    // UTF-8 error beyond 127. Ignoring for now.
+    for xor_byte in 0..127 {
+        let mut character_counts: HashMap<u8, usize> = HashMap::new();
+
+        let result_bytes = bytes_xor_single_byte(&bytes, xor_byte);
+        let result_string = std::str::from_utf8(&result_bytes).unwrap().to_string();
+
+        for byte in result_bytes {
+            let count = character_counts.entry(byte).or_insert(0);
+            *count += 1;
+        }
+
+        fn is_letter(character: u8) -> bool {
+            (character >= 65 && character <= 90) || (character >= 97 && character <=122)
+        }
+
+        let score: usize = character_counts
+            .into_iter()
+            .filter(|(character, _)| is_letter(*character))
+            .map(|(character, count)| character as usize * count)
+            .sum();
+
+        result.insert(result_string, score);
     }
+
+    let message = result.into_iter().max_by_key(|(_, score)| score.clone()).unwrap().0;
+
+    assert_eq!(message, "Cooking MC's like a pound of bacon");
 }
